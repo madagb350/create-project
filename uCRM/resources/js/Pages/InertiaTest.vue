c57a9c5d9a86725dfebdf3a464945d22d8162808
Your Name <you@example.com>
2024-09-01 22:48:02 +0900

    add:install,test

---
+++ uCRM/resources/js/Pages/InertiaTest.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { Link } from "@inertiajs/vue3";
+</script>
+
+<template>
+    Inertiaテストです。<br />
+    <a href="/">aタグ経由です </a><br />
+    <Link href="/">Linkタグ経由です</Link><br />
+    <Link :href="route('inertia.index')">名前付きルート</Link>
+</template>
